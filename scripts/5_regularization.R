8a5f60396a1a12c58ebded6629c922e643211777
Andee Kaplan <andee.kaplan@gmail.com>
2022-10-07 14:51:24 -0600

    try to create lab solution

--- scripts/5_regularization.R
+++ scripts/5_regularization.R
@@ -1,7 +1,6 @@
 library(ISLR)
 library(tidyverse) 
-library(knitr)
-library(glmnet) # package for lasso and ridge regression
+library(tidymodels) 
 
 # data
 str(Hitters)
@@ -10,25 +9,32 @@ str(Hitters)
 set.seed(445)
 
 ## Data Processing
-# 1. Remove records with missing values from the data (Hint: `complete.cases()` is useful)
-# 2. Use `model.matrix` to create an $X$ matrix for all predictors that contains dummy variables for categorical predictors (for predicting `Salary`). You can specify this as a formula in the `model.matrix` call, e.g. 
-#    eg. x <- model.matrix(y ~ ., data)[, -1] # remove the y column
-# 3. Create a $Y$ vector of `Salary` information.
+# 1.  Remove records with missing data. Create a new (complete) version of your data set. (Hint: `drop_na` in `tidyr` could be helpful.)
+# 2. You may need to create dummy variables for categorical variables in your recipes. `step_dummy(all_nominal_predictors())` is a good way to do this.
+# 3. You may need to standardize all variables in your recipes. `step_normalize(all_predictors())` is a good way to do this.
 
 ## Ridge Regression
 # 1. Create a vector of $\lambda$ values from $\lambda = .01$ to  $\lambda = 10^10$ of length $100$.
-# 2. Fit a ridge regression model for each $\lambda$ in your grid.
+# 2. Fit a ridge regression model for each $\lambda$ in your grid. Be sure to normalize your predictors.
 # 3. Make a line plot of coefficient corresponding to each $\lambda$. You should have an individual line for each variable with coefficient value on the $y$-axis and $\lambda$ on the $x$ axis. What happens to your coefficients as $\lambda$ increases?
-# 4. Use `cv.glmnet` to perform $10$-fold cross validation and get an estimate of the test MSE for each $\lambda$ in your grid. Which $\lambda$ would you choose and why?
+# 4. Perform $10$-fold cross validation and get an estimate of the test MSE for each $\lambda$ in your grid. Which $\lambda$ would you choose and why? (Hint: look at the `tune` package for a fast way to do this.)
 
 ## Lasso
 # 1. Fit the lasso model for each $\lambda$ in your grid.
 # 2. Make a line plot of coefficient corresponding to each $\lambda$. You should have an individual line for each variable with coefficient value on the $y$-axis and $\lambda$ on the $x$ axis. (Hint: `coef` may be a useful function). What happens to your coefficients as $\lambda$ increases?
-# 3. Use `cv.glmnet` to perform $10$-fold cross validation and get an estimate of the test MSE for each $\lambda$ in your grid. Which $\lambda$ would you choose and why?
-
+# 3. Perform $10$-fold cross validation and get an estimate of the test MSE for each $\lambda$ in your grid. Which $\lambda$ would you choose and why?
+  
 ## Principal Components Regression
-# 1. Fit the PCR model using the `pcr` command. A couple tips: a) setting `scale = TRUE` will standardize your data prior to fitting the model, and b) setting `validation = TRUE` will perform 10-fold cross validation for each value of $M$.
-# 2. Create a plot of the CV MSE (note root MSE is reported) vs. $M$.
+# 1. Fit the PCR model using 10-fold cross validation for values of $M$. Be sure to normalize your predictors.
+# 2. Create a plot of the CV MSE vs. $M$.
 # 3. When does the smallest cross-validation error occur? Which $M$ would you choose for your final model?
-# 4. The `summary` function also provides the *percentage of variance explained* in the predictors and the response using $M$ principal components. How many principal components would we need to explain at least 80% of the variability in the predictors? 
+# 4. How many principal components would we need to explain at least 80% of the variability in the predictors? 
 # 5. How much variability in $Y$ is explained for your chosen value of $M$?
+
+## Partial Least Squares
+# 1. Fit the PLS model using 10-fold cross validation for values of $M$. Be sure to normalize your predictors.
+# 2. Create a plot of the CV MSE vs. $M$.
+# 3. When does the smallest cross-validation error occur? Which $M$ would you choose for your final model?
+# 4. How many principal components would we need to explain at least 80% of the variability in the predictors? 
+# 5. How much variability in $Y$ is explained for your chosen value of $M$?
+# 6. Discuss the difference between PCR and PLS results. Which would you prefer?
