b6ebb96e14f9304abc61a1f99cb745823e986add
Andee Kaplan <andee.kaplan@gmail.com>
2022-11-07 12:13:51 -0700

    update lab

--- scripts/8_trees.R
+++ scripts/8_trees.R
@@ -1,6 +1,9 @@
 library(ISLR) ## data package
 library(tidyverse) ## data manipulation
+library(tidymodels) ## tidy modeling
 library(knitr) ## tables
+library(rpart.plot) ## tree diagrams
+library(vip) ## plotting variable importance
 
 ## reproducible
 set.seed(445)
@@ -15,28 +18,23 @@ str(Carseats)
 # 4. Remove the `Sales` column from `df`.
 
 ## Decision Trees
-library(tree) ## tree package
-# 1. Using the `tree` function, fit a large classification tree to predict `high_sales` using every variable in `df`. [**Hint:** The syntax is very similar to `lm`]
-# 2. Inspect your tree using `summary`. How many terminal nodes do you have? Whatt is the training error rate?
-# 3. Use the `plot` function to visualize your tree. What is the most important indicator of high sales?
-# **Hint:** Adding the following line after you plot the tree will add labels.
-# text(tree.fit, pretty = 0)
-# 4. Split your observations into a training and a test set with $200$ records each. Estimate the test error rate of your tree. [**Hint:** using `type = "class"` in your `predict` function will get you the actual class predictions.]
+# 1. Using the `decision_tree()` function with the `"rpart"` engine in `tidymodels`, fit a large classification tree to predict `high_sales` using every variable in `df`. [**Hint:** The syntax is very similar fitting a `linear_reg`]
+# 2. Inspect your tree. How many terminal nodes do you have? What is the training error rate?
+# 3. Use the `rpart.plot` function to visualize your tree. What is the most important indicator of high sales?
+# 4. Split your observations into a training and a test set with $200$ records each. Estimate the test error rate of your tree. 
 # 5. Produce a confusion matrix for your test set.
-# 6. Use the `cv.tree` function to perform cross-validation to determine the optimal level of tree complexity. Using `FUN = prune.misclass` indicates that we want to use the classification error rate (instead of deviance) to guide the CV and pruning process. Which $\alpha$ (corresponds to `k` in the output) should we choose?
-# 7. Use the function `prune.misclass` to prune your tree to the chosen complexity.
+# 6. Perform cross-validation to determine the optimal level of tree complexity on your training data set. Which $\alpha$ (corresponds to `k` in the output) should we choose?
+# 7. Use the functions `finalize_workflow` and `select_best()` to prune your tree to the chosen complexity. Plot your final tree.
 # 8. Repeat 4-5 using your pruned tree. Which performs better?
 
 ## Bagging & Random Forests
-library(randomForest) # random forests & bagging
 # 1. Perform bagging on your training `df` to predict `high_sales`. Specify `importance = TRUE` to also obtain information on the importance of each predictor.
-# 2. Make a plot of the importance values for each predictor. What is the predictor with the highes importance?
+# 2. Make a plot of the importance values for each predictor using the `vip` function. What is the predictor with the highest importance?
 # 3. Estimate the test error rate using your bagged tree model.
-# 4. Repeat 1-3 using a random forest with $m = \sqrt{p}$.
-# 5. Compare the OOB confusion matrix to your test confusion matrix. [**Hint:** The `confusion` element of the model output is OOB.]
+# 4. Repeat 1-3 using a random forest with $m = \sqrt{p}$ via `mtry = sqrt(.cols())`.
+# 5. Compare the OOB confusion matrix to your test confusion matrix. [**Hint:** The `confusion` element of the model output fit is OOB.]
 
 ## Boosting
-library(gbm) ## boosting package
-# 1. Fit a boosted tree ensemble to your training `df` predicting `high_sales` with $B = 5,000$ trees, shrinkage parameter of $\lambda = 0.1$, and an interaction depth of $d = 2$. We sure to include `distribution = "bernoulli"` to indicate a classification problem.
+# 1. Fit a boosted tree ensemble to your training `df` predicting `high_sales` with $B = 5,000$ trees, learning rate of $\lambda = 0.1$, and an interaction depth of $d = 2$. 
 # 2. Estimate the test error rate using your boosted tree model and compare to all previously fit models.
   
